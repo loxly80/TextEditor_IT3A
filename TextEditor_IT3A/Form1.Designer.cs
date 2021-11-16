bd330a3443de3e3b62a8930a6dddc61f0bdb8884
Jiří Hlaváček <Hlavacek.Jiri@vassboskovice.cz>
2021-11-16 13:45:53 +0100

    Hotovo

--- TextEditor_IT3A/Form1.Designer.cs
+++ TextEditor_IT3A/Form1.Designer.cs
@@ -29,20 +29,47 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnSaveAs = new System.Windows.Forms.Button();
             this.btnOpen = new System.Windows.Forms.Button();
             this.txtContent = new System.Windows.Forms.TextBox();
             this.ofd = new System.Windows.Forms.OpenFileDialog();
+            this.sfd = new System.Windows.Forms.SaveFileDialog();
             this.panel1.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
+            this.panel1.Controls.Add(this.btnSave);
+            this.panel1.Controls.Add(this.btnSaveAs);
             this.panel1.Controls.Add(this.btnOpen);
             this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
             this.panel1.Size = new System.Drawing.Size(888, 41);
             this.panel1.TabIndex = 0;
+            // 
+            // btnSave
+            // 
+            this.btnSave.Enabled = false;
+            this.btnSave.Location = new System.Drawing.Point(121, 3);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(112, 34);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Uložit";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // btnSaveAs
+            // 
+            this.btnSaveAs.Enabled = false;
+            this.btnSaveAs.Location = new System.Drawing.Point(239, 3);
+            this.btnSaveAs.Name = "btnSaveAs";
+            this.btnSaveAs.Size = new System.Drawing.Size(112, 34);
+            this.btnSaveAs.TabIndex = 0;
+            this.btnSaveAs.Text = "Uložit jako";
+            this.btnSaveAs.UseVisualStyleBackColor = true;
+            this.btnSaveAs.Click += new System.EventHandler(this.btnSaveAs_Click);
             // 
             // btnOpen
             // 
@@ -60,6 +87,7 @@
             this.txtContent.Location = new System.Drawing.Point(0, 41);
             this.txtContent.Multiline = true;
             this.txtContent.Name = "txtContent";
+            this.txtContent.ScrollBars = System.Windows.Forms.ScrollBars.Both;
             this.txtContent.Size = new System.Drawing.Size(888, 526);
             this.txtContent.TabIndex = 1;
             this.txtContent.TextChanged += new System.EventHandler(this.txtContent_TextChanged);
@@ -77,6 +105,7 @@
             this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
             this.panel1.ResumeLayout(false);
             this.ResumeLayout(false);
             this.PerformLayout();
@@ -89,6 +118,9 @@
         private System.Windows.Forms.Button btnOpen;
         private System.Windows.Forms.TextBox txtContent;
         private System.Windows.Forms.OpenFileDialog ofd;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnSaveAs;
+        private System.Windows.Forms.SaveFileDialog sfd;
     }
 }
 
--- TextEditor_IT3A/Form1.cs
+++ TextEditor_IT3A/Form1.cs
@@ -7,15 +7,29 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
+using Newtonsoft.Json;
 
 namespace TextEditor_IT3A
 {
     public partial class Form1 : Form
     {
         string filePath;
+        string configFilePath = "config.cfg";
+
         public Form1()
         {
             InitializeComponent();
+            if (File.Exists(configFilePath))
+            {
+                filePath = File.ReadAllText(configFilePath);
+                if (File.Exists(filePath))
+                {
+                    txtContent.Text = File.ReadAllText(filePath);
+                    btnSave.Enabled = true;
+                    btnSaveAs.Enabled = true;
+                }
+            }
         }
 
         private void txtContent_TextChanged(object sender, EventArgs e)
@@ -31,7 +45,41 @@ namespace TextEditor_IT3A
             {
                 filePath = ofd.FileName;
                 Text = filePath;
+                if (File.Exists(filePath))
+                {
+                    txtContent.Text = File.ReadAllText(filePath);
+                    btnSave.Enabled = true;
+                    btnSaveAs.Enabled = true;
+                }
             }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            File.WriteAllText(filePath, txtContent.Text);
+        }
+
+        private void btnSaveAs_Click(object sender, EventArgs e)
+        {
+            sfd.FileName = "NewFile.txt";
+            sfd.Filter = "Text file|*.txt";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                filePath = sfd.FileName;
+                Text = filePath;
+                File.WriteAllText(filePath, txtContent.Text);
+            }
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            var result = MessageBox.Show("Opravdu chcete ukončit bez uložení?","Zavírání aplikace",MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (result == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+            //var json = JsonConvert.SerializeObject(filePath);
+            File.WriteAllText(configFilePath,filePath);
         }
     }
 }
